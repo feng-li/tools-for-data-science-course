867e0eab332594e9d6d0b780141ca189fdcda060
unknown <shenxinlan1998@126.com>
2019-04-17 18:56:26 +0800

    add five_examples

--- 2016311178/find_root.py
+++ 2016311178/find_root.py
@@ -2,14 +2,12 @@ def qua():
     """
     find the roots of a random equation
     """
-
     import math
     import random
     a = random.randint(1,100)
     b = random.randint(-100,100)
     c = random.randint(-100,100)
     print("a=",a,"b=",b,"c=",c) #生成随机数a,b,c
-    
     delta= b**2 - 4*a*c
     if delta < 0:
         print("方程无实数根")
@@ -20,3 +18,4 @@ def qua():
         x1 = (-b + math.sqrt(delta))/(2*a)
         x2 = (-b - math.sqrt(delta))/(2*a)
         print("方程有两个不同根，分别为x1=",x1,"x2=",x2)
+
