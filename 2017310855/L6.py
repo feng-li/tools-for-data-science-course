7e4d2e22ba5d4ddaf9d6f932b3af5a06284c1936
Jiachen-Mu <48760268+Jiachen-Mu@users.noreply.github.com>
2019-04-25 09:08:48 +0800

    Update L6.py

--- 2017310855/L6.py
+++ 2017310855/L6.py
@@ -3,4 +3,4 @@ import seaborn as sns
 # 调用泰坦尼克数据集
 data = sns.load_dataset("titanic")
 # logit回归
-sns.lmplot(x = "age", y = "survived", hue = "sex", col = "pclass", data = data,logistic = True)
+sns.lmplot(x = "age", y = "survived", hue = "sex", col = "pclass", data = data, logistic = True)
