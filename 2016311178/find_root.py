72b0390d56dd541fc301d9efedb506a727b48fb5
unknown <shenxinlan1998@126.com>
2019-05-15 22:01:35 +0800

    add scrapy

--- 2016311178/find_root.py
+++ 2016311178/find_root.py
@@ -1,21 +1,28 @@
 def qua():
+
     """
+
     find the roots of a random equation
+
     """
+
     import math
     import random
     a = random.randint(1,100)
     b = random.randint(-100,100)
     c = random.randint(-100,100)
     print("a=",a,"b=",b,"c=",c) #生成随机数a,b,c
-    delta= b**2 - 4*a*c
-    if delta < 0:
-        print("方程无实数根")
-    elif delta == 0:
-        x = -b/2*a
-        print("方程只有同一个实数根，为",x)
-    else:
-        x1 = (-b + math.sqrt(delta))/(2*a)
-        x2 = (-b - math.sqrt(delta))/(2*a)
-        print("方程有两个不同根，分别为x1=",x1,"x2=",x2)
 
+    delta= b**2 - 4*a*c
+    if a == 0:
+        print("方程不是一元二次方程")
+    else:
+        if delta < 0:
+            print("方程无实数根")
+        elif delta == 0:
+            x = -b/2*a
+            print("方程只有同一个实数根，为",x)
+        else:
+            x1 = (-b + math.sqrt(delta))/(2*a)
+            x2 = (-b - math.sqrt(delta))/(2*a)
+            print("方程有两个不同根，分别为x1=",x1,"x2=",x2)
