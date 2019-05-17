0d4442e280b0c847ef69bac76b688f8365d75fa4
Sijia-Gao <1423231596@qq.com>
2019-05-17 18:00:24 +0800

    采集总体页数

--- 2017310895/payapa.py
+++ 2017310895/payapa.py
@@ -71,3 +71,6 @@ for i in range(3):
    #total={"Title":title,"Article":article,"SourceandTime":source_time}
    #df=pandas.DataFrame(total)
    #df.to_excel('news.xlsx')"""
+import logging  # 引入logging模块
+logging.basicConfig(level=logging.NOTSET)  # 设置日志级别
+logging.debug(u"如果设置了日志级别为NOTSET,那么这里可以采取debug、info的级别的内容也可以显示在控制台上了")
