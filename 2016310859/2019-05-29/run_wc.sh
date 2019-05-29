a198962302e79c10c5d781133e9eb981b8fbdf7e
郝建锋 <1246574158@qq.com>
2019-05-29 21:17:10 +0800

    Add run_wc.sh

---
+++ 2016310859/2019-05-29/run_wc.sh
@@ -0,0 +1,9 @@
+hadoop fs -rm -r /00lifeng/HJF/output/
+
+hadoop jar $HADOOP_HOME/share/hadoop/tools/lib/hadoop-streaming-2.7.2.jar \
+       -input /00lifeng/HJF/300241_match_New.csv \
+       -output /00lifeng/HJF/output/ \
+       -mapper "/usr/bin/cat" \
+       -reducer "/bin/wc"
+
+hadoop fs -cat /00lifeng/HJF/output/*
