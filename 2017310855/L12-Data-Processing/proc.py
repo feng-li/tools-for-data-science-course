f70d039738efb76fddc3460d4123ef18ffb3876f
牟家辰 <48760268+Jiachen-Mu@users.noreply.github.com>
2019-06-13 20:15:53 +0800

    Add join

--- 2017310855/L12-Data-Processing/proc.py
+++ 2017310855/L12-Data-Processing/proc.py
@@ -6,17 +6,17 @@ import io
 input = io.TextIOWrapper(sys.stdin.buffer, encoding = 'gbk')
 
 for line in input:
-
+	
     # Split
-	field = line.split(',')
+    field = line.split(',')
     
     # Process
     date = field[2]
     max = float(field[5])
     min = float(field[4])
     range = max - min
-	mean = (max + min)/2
+    mean = (max + min)/2
 
-    data = (date, max, min, range, mean)
-    print(data) 
+    data = (date, str(max), str(min), str(range), str(mean))
+    ''.join(data)
     
