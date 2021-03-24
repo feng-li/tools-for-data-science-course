b31c27e0b04ef894fe6c2f1e79604a0c6e07a6c0
issacfeynman <79582231+issacfeynman@users.noreply.github.com>
2021-03-24 21:04:44 +0800

    Update hello.py

--- 2021spring/2020311538/hello.py
+++ 2021spring/2020311538/hello.py
@@ -1,3 +1,10 @@
-a=1
-b=2
-print(a+b)
+def leap year check(n):
+    '''to judge whether the year is a leap year'''
+    if n%100=0
+        if n%400=0
+            print(true)
+        else print(false)
+    elif n%4=0
+        print(true)
+    else print(false)
+    return(I'm done)
