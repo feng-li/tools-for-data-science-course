86abb95db73d1e2f140f11c6515f755af46b2304
Lehan-King <79582243+Lehan-King@users.noreply.github.com>
2021-03-31 17:32:38 +0800

    Add files via upload

---
+++ 2021spring/2020311079/闰年作业.py
@@ -0,0 +1,131 @@
+Python 3.9.2 (tags/v3.9.2:1a79785, Feb 19 2021, 13:44:55) [MSC v.1928 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> def year()
+SyntaxError: invalid syntax
+>>> def year():
+	year=input('请输入一个年份:')
+	year=int(year)
+	if year%400==0
+	
+SyntaxError: invalid syntax
+>>> def year():
+	year=input('请输入一个年份:')
+	year=int(year)
+	if year%400==0:
+		print('年份是闰年')
+		elif year%4=0 and year%100==0:
+			
+SyntaxError: invalid syntax
+>>> def year():
+	year=input('请输入一个年份:')
+	year=int(year)
+	if year%400==0:
+		print('年份是闰年')
+		elif year%4==0 and year%100==0:
+			
+SyntaxError: invalid syntax
+>>> def year():
+	year=input('请输入一个年份:')
+	year=int(year)
+	if year%400==0:
+		print('年份是闰年')
+		elif year%4==0 and year%100!=0:
+			
+SyntaxError: invalid syntax
+>>> def year():
+	year=input('请输入一个年份:')
+	year=int(year)
+	if year%400==0:
+		print('年份是闰年')
+		elif year%4==0 and year%100!=0:
+			
+SyntaxError: invalid syntax
+>>> def year():
+	year=input('请输入一个年份:')
+	year=int(year)
+	if year%400==0:
+		print('年份是闰年')
+		elif year % 4== 0 and year % 100 ==0:
+			
+SyntaxError: invalid syntax
+>>> def year():
+	year=input('请输入一个年份:')
+	year=int(year)
+	if year%4==0
+	
+SyntaxError: invalid syntax
+>>> def year():
+	year=input('请输入一个年份:')
+	year=int(year)
+	if year%4==0:
+		if year%100==0:
+			if year%400==0:
+				print("年份是闰年")
+				else print("年份不是闰年")
+				
+SyntaxError: invalid syntax
+>>> def year():
+	year=input('请输入一个年份:')
+	year=int(year)
+	if year%4==0:
+		if year%100==0:
+			if year%400==0:
+				print("年份是闰年")
+				else: print("年份不是闰年")
+				
+SyntaxError: invalid syntax
+>>> def year():
+	year=input('请输入一个年份:')
+	year=int(year)
+	if year%4==0:
+		if year%100==0:
+			if year%400==0:
+				print("年份是闰年")
+				else:
+					
+SyntaxError: invalid syntax
+>>> def year():
+	year=input('请输入一个年份:')
+	year=int(year)
+	if year%4==0:
+		if year%100==0:
+			if year%400==0:
+				print("年份是闰年")
+				el:
+					
+SyntaxError: invalid syntax
+>>> def year():
+	year=input('请输入一个年份:')
+	year=int(year)
+	if year%4==0:
+		if year%100==0:
+			if year%400==0:
+				print("年份是闰年")
+				else:
+					
+SyntaxError: invalid syntax
+>>> def year():
+	year=input('请输入一个年份:')
+	year=int(year)
+	if year%4==0:
+		if year%400==0:
+		  print("年份是闰年")
+		elif year%100==0:
+			print("年份不是闰年")
+		else:
+			print("年份是闰年")
+	else:
+		print("年份不是闰年")
+
+
+
+
+
+		
+
+
+
+
+
+
+		
