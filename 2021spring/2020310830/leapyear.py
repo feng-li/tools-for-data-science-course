6c5cb7cd7c6f3ad4d7cd314cd3ae86a3be746b31
wocufer <81303079+wocufer@users.noreply.github.com>
2021-04-04 17:22:21 +0800

    def

--- 2021spring/2020310830/leapyear.py
+++ 2021spring/2020310830/leapyear.py
@@ -1,13 +1,15 @@
+def leapyear(year):
+    if (year % 4 ) == 0:
+        if (year % 100 ) == 0:
+            if (year % 400 ) == 0:
+                print("{0}是闰年".format(year))
+            else:
+                print("{0}不是闰年".format(year))
+        else:
+            print("{0}不是闰年".format(year))
+    else:
+        print("{0}不是闰年".format(year))
 
- 
-year = int(input("输入一个年份: "))
-if (year % 4) == 0:
-   if (year % 100) == 0:
-       if (year % 400) == 0:
-           print("{0} 是闰年".format(year))   # 整百年能被400整除的是闰年
-       else:
-           print("{0} 不是闰年".format(year))
-   else:
-       print("{0} 是闰年".format(year))       # 非整百年能被4整除的为闰年
-else:
-   print("{0} 不是闰年".format(year))
+
+year = int(input("输入一个年份："))
+leapyear(year)
