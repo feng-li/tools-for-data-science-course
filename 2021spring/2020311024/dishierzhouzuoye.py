abc64568b4c01ac0361b1aeefadb7f31a0d51d17
Wzineng <79582287+Wzineng@users.noreply.github.com>
2021-05-12 18:16:07 +0800

    Add files via upload

---
+++ 2021spring/2020311024/dishierzhouzuoye.py
@@ -0,0 +1,7 @@
+s="This is the first time that innovation comes first in a theoretical framework moreover it encompasses diverse kinds of innovation obviously in science and technology but also in management and processes Innovation is technological industrial institutional and cultural "
+def split(s):
+    return s.split("")
+print(s.split())
+
+
+
